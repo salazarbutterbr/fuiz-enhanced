4d9c7208b2ce379e6b08bab4885610bfc7babe07
Samarth Vohra <samarthvohra@Samarths-Mac.local>
2025-08-08 15:00:43 +0530

    Fix svelte.config.js: Remove missing dependencies and simplify configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,11 @@
 import adapter from '@sveltejs/adapter-node';
-import { importAssets } from 'svelte-preprocess-import-assets';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
+	extensions: ['.svelte'],
 	preprocess: [
-		importAssets({
-			sources: (defaultSources) => {
-				return [
-					...defaultSources,
-					{
-						tag: 'Icon',
-						srcAttributes: ['src']
-					},
-					{
-						tag: 'IconButton',
-						srcAttributes: ['src']
-					}
-				];
-			}
-		}),
-		vitePreprocess(),
-		mdsvex({
-			extensions: ['.md']
-		})
+		vitePreprocess()
 	],
 
 	compilerOptions: {
