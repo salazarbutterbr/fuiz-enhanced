063f52c9167aca6d27d5844f73843e4b8e8546f4
Samarth Vohra <samarthvohra@Samarths-Mac.local>
2025-08-08 14:41:48 +0530

    Remove enhanced-img dependency to fix build error - simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		enhancedImages(),
 		paraglide({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
